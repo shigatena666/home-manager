2388a87c91e8a3d411456cfa7b6e388be93027a2
shigatena <50152760+shigatena666@users.noreply.github.com>
2024-08-04 08:26:07 +0200

    Update brew.nix

--- nixosModules/brew.nix
+++ nixosModules/brew.nix
@@ -1,33 +1,23 @@
 { pkgs, lib, config, ... }:
 
-let
-  cfg = config.brew;
-in
 {
   options.brew = {
     enable = lib.mkEnableOption "enables brew module";
   };
 
-  config = lib.mkIf cfg.enable {
-    home.file.".Brewfile".text = ''
-      tap "homebrew/cask"
+  config = lib.mkIf config.brew.enable {
+    homebrew = {
+      taps = [
+        "homebrew/cask"
+      ];
 
-      # brews
-      # brew "ani-cli"
+      brews = [
 
-      # casks
-      # cask arc
-    '';
+      ];
 
-    home.activation.brewInstall = lib.hm.dag.entryAfter ["writeBoundary"] ''
-      if command -v brew >/dev/null 2>&1; then
-        echo "Updating Homebrew..."
-        brew update
-        echo "Installing Homebrew packages..."
-        brew bundle --file=${config.home.homeDirectory}/.Brewfile
-      else
-        echo "Homebrew is not installed. Please install Homebrew first."
-      fi
-    '';
+      casks = [
+
+      ];
+    };
   };
 }
